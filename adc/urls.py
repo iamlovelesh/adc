30835fe9a3431f5f6feb257720c964a3bfc45820
iamlovelesh <rghvn29@gmail.com>
2021-08-11 00:11:22 +0530

    Added accounts utilities

--- adc/urls.py
+++ adc/urls.py
@@ -25,4 +25,34 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("thanks/", views.LogoutRedirectView.as_view(), name="thanks"),
     path("signup/", views.SignUp.as_view(), name="signup"),
+    path(
+        "password_change/",
+        auth_views.PasswordChangeView.as_view(template_name="registration/password_change_form.html"),
+        name="password_change",
+    ),
+    path(
+        "password_change_done/",
+        auth_views.PasswordChangeDoneView.as_view(template_name="registration/password_change_done.html"),
+        name="password_change_done",
+    ),
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html"),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"),
+        name="password_reset_complete",
+    ),
 ]
